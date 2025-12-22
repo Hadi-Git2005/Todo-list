78673ca482a2b518daaee1f8f10e9b206ac036ba
Hadi-Git2005 <rasoolhadi97@gmail.com>
2025-12-22 21:50:22 +0530

    Slightly Styled Version Of ToDo Page

--- src/pages/Todopages.jsx
+++ src/pages/Todopages.jsx
@@ -2,36 +2,37 @@ import { useState } from "react";
 
 function Todopages()
 {
-    const[input,setInput] = useState("");
-    const[items,setItems] = useState([]);
-    function handleAdd()
-    {
-        if(input.trim()!=" ")
+    const[input,setInput] = useState("")
+    const[items,setItems] = useState([])
+    function handleClick(){
+        if(input.trim()!="")
         {
             items.push(input + " ")
             setItems([...items])
-            setInput("");
+            setInput("")
+        }
+        else{
+            return
         }
     }
     return(
         <>
-            <main>
-                <h1>Welcome to ToDo</h1>
-                <section>
-                    <input value={input} onChange={(e) => setInput(e.target.value)}  type="text"/>
-                    <button onClick = {()=>handleAdd()}>add</button>
-                </section>
-                <section>
-                    <ol>
+        <main className="flex flex-col items-center">
+            <h1 className="text-center rounded-md bg-teal-400 w-fit px-3 py-1"><strong>Welcome ToDo List</strong></h1>
+            <br></br>
+            <section>
+                    <input className="text-teal-300" type="text" onChange={(e)=>setInput(e.target.value)} value = {input}/>
+                    <button className="bg-teal-300" onClick={handleClick}> add </button>
+            </section>
+            <section>
+                <ol>
                     {
-                        items.map((val,idx)=>{
-                            return <li>{val}</li>
-                        })
+                        items.map((val,idx)=>{return (<li>{idx + 1}. {val}</li>)})
                     }
-                    </ol>
-                </section>
-            </main>
+                </ol>
+            </section>
+        </main>
         </>
     )
-};
-export default Todopages;
+}
+export default Todopages
